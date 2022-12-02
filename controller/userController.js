e9e8d0c4506fd6e2f89f45a33edcc48b0d55945c
evanzzer <dabestevanzzacc@gmail.com>
2022-12-02 21:59:23 +0700

    Update file and .gitignore

--- controller/userController.js
+++ controller/userController.js
@@ -17,20 +17,20 @@ const register = async(req, res, next) => {
     };
 
     console.log(user);
-    
+
     if (!user.password || !user.email || !user.username) {
         return res.status(400).send({ message : 'email, username, password must be provided'});
     }
 
-    db.query(`SELECT * FROM unhan_modul_17 where email = $1`, [user.email], (err, results) => {
+    db.query(`SELECT * FROM users where email = $1`, [user.email], (err, results) => {
         if (results == null || results.rows.length == 0)  {
-            db.query(`INSERT INTO unhan_modul_17 (username, email, password) values ($1, $2, $3)`, [user.username, user.email, user.password], (err, results) => {
+            db.query(`INSERT INTO users (username, email, password) values ($1, $2, $3)`, [user.username, user.email, user.password], (err, results) => {
                 if (err){
                     console.log("Data Ga masuk: ", err);
                     return res.status(400).send("Data Tidak Masuk")
                 }
                 return res.status(200).send({user})
-            }) 
+            })
         } else {
             return res.status(400).json({message: "Registrasi Gagal"});
         }
@@ -49,10 +49,10 @@ const login = async(req, res, next) => {
         return res.status(400).send({ message : 'email and password is provided'});
     }
 
-    db.query(`SELECT * FROM unhan_modul_17 where email = $1`, [user.email], (err, results) => {
+    db.query(`SELECT * FROM users where email = $1`, [user.email], (err, results) => {
         if (results != null && results.rows.length > 0)  {
             for (let row of results.rows){
-                var hasil = row['email'] 
+                var hasil = row['email']
             }
             if (hasil == null) {
                 console.log('Email Belum Terdaftar');
@@ -63,24 +63,24 @@ const login = async(req, res, next) => {
                 if (!bcrypt.compareSync(user.password, results.rows[0].password)){
                     res.status(400).send({ message : 'The credentials you provided is incorrect' })
                 }
-    
+
                 console.log('berhasil login');
                 const token = jwt.sign(
                     {
                         id: results.rows[0].id,
                         email: user.email,
-                    }, 
+                    },
                         SECRET, { expiresIn: '1d'}
                 )
-    
+
                 console.log({token});
-    
+
                 const user1 = {
                     id: results.rows[0].id,
                     email: results.rows[0].email,
                     username: results.rows[0].username,
                 }
-    
+
                 return res.status(200).json({
                     user : user1,
                     token: token
@@ -89,30 +89,30 @@ const login = async(req, res, next) => {
         } else {
             return res.status(400).json({message: "Email Belum terdaftar"});
         }
-    }) 
+    })
 }
 
 const logout = async(req, res, next) => {
-                
+
     try {
         console.log('berhasil logout');
-        return res.status(200).send('berhasil logout dari aplikasi')   
+        return res.status(200).send('berhasil logout dari aplikasi')
     } catch (err) {
         console.log(err.message);
         return res.status(500).send(err)
     }
-            
+
 }
 
 const verify = async(req, res, next) => {
     try {
         const token = req.body.jwt
         const decoded = jwt.verify(token, SECRET)
-        const user = await db.query(`SELECT * FROM unhan_modul_17 where id = $1`, [decoded.id])
+        const user = await db.query(`SELECT * FROM users where id = $1`, [decoded.id])
         res.status(200).json(user.rows[0])
     } catch (err) {
         console.log(err.message);
-        return res.status(500).send(err)    
+        return res.status(500).send(err)
     }
 }
 
